0ae90dcd8d74ee46abf2eef05e5405264568bc6a
Jakub <77514091+Coderbeep@users.noreply.github.com>
2024-05-17 17:09:52 +0200

    card testing

--- ui.R
+++ ui.R
@@ -1,17 +1,26 @@
 library(shiny)
+library(bslib)
 
 pokemon_selector <- function(num){
     selectizeInput(paste("pokeName",num, sep=""),label = "Choose your pokemon:",choices = NULL)
 }
 
-fluidPage(
-    titlePanel("PokeDex"),
-    pokemon_selector('01'),
-    pokemon_selector('02'),
-    pokemon_selector('03'),
-    pokemon_selector('04'),
-    pokemon_selector('05'),
-    pokemon_selector('06'),
-    plotOutput("avg_stat")
+ui <- fluidPage(
+  titlePanel("PokeDex"),
+  fluidRow(
+    column(2, pokemon_selector('01')),
+    column(2, pokemon_selector('02')),
+    column(2, pokemon_selector('03')),
+    column(2, pokemon_selector('04')),
+    column(2, pokemon_selector('05')),
+    column(2, pokemon_selector('06'))
+  ),
+  plotOutput("avg_stat"),
+  card(
+    card_header(
+      class = "bg-dark",
+      "A header"
+    ),
+    markdown("Some text with a [link](https://github.com).")
+  )
 )
-
